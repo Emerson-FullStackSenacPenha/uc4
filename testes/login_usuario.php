12c74a689683c4d0b87a3cbafad45a6bf0da73dc
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-09-22 19:27:36 -0300

    testando com html

--- testes/login_usuario.php
+++ testes/login_usuario.php
@@ -1,5 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Teste de Login</title>
+</head>
+<body>
+    
+    <h1>Olá, tudo bom ?</h1>
+
+    <input type="text">
+
+
+</body>
+</html>
+
+
+
+
+
+
 <?php
-
-echo "Ola";
-
+// http://localhost/uc4/testes/validar.php
+// http://localhost/uc4/testes/login_usuario.php
 ?>
---
+++ testes/validar.php
@@ -0,0 +1,20 @@
+<?php
+
+    function verificar_senha($senha){
+
+        if( $senha == 1234 ){
+
+            echo "Deu certo";
+
+        }else{
+
+            echo "Deu ruim";
+
+        }
+
+
+    }
+
+    verificar_senha(1234);
+
+?>
