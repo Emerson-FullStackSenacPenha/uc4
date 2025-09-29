fe745066a90d27eb99da469e386c3835f15636c8
Emerson-FullStackSenacPenha <emerson.agomes1@senacsp.edu.br>
2025-09-29 14:36:04 -0300

    criando conteudo de data_base

--- nivel_cafe/database.php
+++ nivel_cafe/database.php
@@ -1,5 +1,43 @@
 <?php
 
-    
+    $ninjas = [
+
+        $ninja1 = [
+
+            'nome' => "Jonas",
+            'cla' => "Muce",
+            'nivel' => 1,
+            'missoes' => [ "Jornada", "Futebol", "Natação" ],
+            'filhos' => false
+
+        ],
+
+        $ninja2 = [
+
+            'nome' => "Claudia",
+            'cla' => "Korn",
+            'nivel' => 2,
+            'missoes' => [ "Busca", "Natação", "Fugir" ],
+            'filhos' => true
+
+        ],
+
+        $ninja3 = [
+
+            'nome' => "Jonas",
+            'cla' => "Dre",
+            'nivel' => 3,
+            'missoes' => [ "Perigo", "Corrida", "Escorregar" ],
+            'filhos' => false
+
+        ]
+
+    ];
+
+    /* Exibindo em tela
+    echo $ninjas[0]["missoes"][1]."<br>";
+    echo $ninjas[2]['filhos']."<br>";
+    echo $ninjas[2]["cla"]."<br>";
+    */
 
 ?>
